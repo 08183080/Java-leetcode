9256f32336a43e466dc14fad64324eb30ed903bf
龙龙 <51738561+08183080@users.noreply.github.com>
2023-09-25 13:01:16 +0800

    完善单链表的增加节点算法

--- src/Linked_List/SinglyList.java
+++ src/Linked_List/SinglyList.java
@@ -3,6 +3,10 @@ package src.Linked_List;
 // 107
 //
 //If all of your classes are in the same package, you shouldn't need to import them.
+// ref: https://aaronice.gitbook.io/lintcode/linked_list
+/*
+不走,永远不会犯错,但是不犯错,永远不会成长的。。。
+**/
 public class SinglyList {
     SinglyListNode head;
     SinglyList (SinglyListNode node) {
@@ -17,6 +21,27 @@ public class SinglyList {
         prev.next = cur;
         return head;
     }
+
+    // 在head之前增加节点...
+    SinglyListNode addBeforeHead(int val) {
+        SinglyListNode node = new SinglyListNode(val);
+        node.next = head;
+        head = node;
+        return head;
+    }
+
+    // 在tail之后增加节点
+    SinglyListNode addAfterTail(int val) {
+        SinglyListNode node = new SinglyListNode(val);
+        SinglyListNode cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+        return node;
+    }
+
+
 
     // 遍历整个链表...
     void  travel(SinglyListNode head) {
@@ -33,7 +58,9 @@ public class SinglyList {
         SinglyListNode head = new SinglyListNode(6);
         SinglyList list = new SinglyList(head);
         list.addNode(head, 3);
-        list.addNode(head, 2);
+        list.addNode(head, 2);// 6 -> 2 -> 3
+        head = list.addBeforeHead(1); // 1 -> 6 -> 2 -> 3  // 重新设置头节点
+        list.addAfterTail(100); // 1 -> 6 -> 2 ->3 -> 100
         list.travel(head);
     }
 }
