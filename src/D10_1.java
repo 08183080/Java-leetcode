98a0509e04b43cf61aa8842684c593e61302b605
龙龙 <51738561+08183080@users.noreply.github.com>
2023-10-01 10:29:05 +0800

    dp

---
+++ src/D10_1.java
@@ -0,0 +1,14 @@
+package src;public class D10_1 {
+    public int maxProfit(int[] prices) {
+        // dp[i] = nums[i] - min(nums[0...i-1])
+        // max(dp[0...n-1])
+        int n = prices.length;
+        int mmax = 0;
+        int minn = prices[0];
+        for (int i = 1; i < n; i++) {
+            minn = Math.min(minn, prices[i-1]);
+            mmax = Math.max(mmax, prices[i] - minn);
+        }
+        return mmax;
+    }
+}
