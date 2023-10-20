ec9d57ea83aa7deb1122fc22104b8e8f3b4725ae
龙龙 <51738561+08183080@users.noreply.github.com>
2023-10-20 22:47:25 +0800

    java

---
+++ src/leetcode/life.java
@@ -0,0 +1,13 @@
+package src.leetcode;
+
+public class life {
+    public static void everyday() {
+        for (int i = 0; i < 30000; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        everyday();
+    }
+}
